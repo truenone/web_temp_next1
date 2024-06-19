49cc9acac45036778189a618470e478bb86b3523
truenone <akose5067@gmail.com>
2024-06-19 23:42:46 +0300

    Article card grubu güncellendi

--- src/components/ArticleCardGroup/index.tsx
+++ src/components/ArticleCardGroup/index.tsx
@@ -3,10 +3,15 @@ import { ArticleCardImage } from '../ArticleCard'
 
 function ArticleCardGroup() {
     return (
-        <div className="flex flex-row gap-5 h-[500px] justify-center w-full pt-10">
-            <ArticleCardImage category="Boya Badana" />
-            <ArticleCardImage category="Tadilat" />
-            <ArticleCardImage category="Parke" />
+        <div className='flex flex-col gap-2 justify-center items-center'>
+            <div>
+                <h2 className='font-extrabold text-5xl'>Örnek Projeler</h2>
+            </div>
+            <div className="flex flex-row gap-5 h-[500px] justify-center w-full pt-10">
+                <ArticleCardImage category="Boya Badana" />
+                <ArticleCardImage category="Tadilat" />
+                <ArticleCardImage category="Parke" />
+            </div>
         </div>
     )
 }
